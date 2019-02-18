299c17b68218710f7c28d478b3c649a9c9310bf3
Mrityunjoy <Mrityunjoy@MacBook-Pro.local>
2019-02-18 12:28:57 +0600

    Basic App

---
+++ Brain Station 23/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Mrityunjoy on 2/13/19.
+//  Copyright © 2019 Moyeen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
